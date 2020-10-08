c569146b196eb77d191dd857cbcca759444ac0dd
Andy Greenhaw <andy.greenhaw@gmail.com>
2020-10-08 03:04:06 -0600

    Got everything working except making the buttons dissappear and Local Storaging the Forecast data

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ var apiKey = "e7d65f8500681df1e3559a6964e703f1";
 
 var ajaxRequest;
 
+var weatherIconCode;
 var weatherIcon;
 var tempNow;
 var humidNow;
@@ -19,6 +20,8 @@ var windNow;
 var latC;
 var lonC;
 var uvIndex;
+
+var currentDate;
 
 /////////////////////////////////////
 // LINK HTML SECTIONS TO VARIABLES //
@@ -30,6 +33,15 @@ var forecastSection = $("#forecast");
 var searchSection = $("#search");
 var recentSearchSection = $("#navBar");
 var bodySection = $("#body");
+var tempDisplay = $("#tempDisplay");
+var humidDisplay = $("#humidDisplay");
+var windDisplay = $("#windDisplay");
+var uvDisplay = $("#uvIndexDisplay");
+var day1 = $("#day1")
+var day2 = $("#day2")
+var day3 = $("#day3")
+var day4 = $("#day4")
+var day5 = $("#day5")
 
 //////////////////////////
 // Lay Down a Header //
@@ -41,10 +53,6 @@ headDiv.append(heroHeadline);
 //////////////////////////
 // BUILD SEARCH SECTION //
 //////////////////////////
-
-// Set Up Array for Recent Searches
-var recentSearchArray = [];
-console.log(recentSearchArray);
 
 // Create Search Headline, Input, and Button//
 searchHeadline = $("<h3>").text("Search for a City:");
@@ -60,68 +68,178 @@ searchButton.on("click", searchButtonFunction);
 function searchButtonFunction(e){
     e.preventDefault;
 
-    //Send User Entries to First Ajax Function and Push to Recent Search Array//
-    var recentSearch = $(this).siblings("#searchInput").val();
-    recentSearchArray.push(recentSearch);
-    ajaxRequest = $(this).siblings("#searchInput").val();
-
-        //Create List of Buttons with Recent Searches
-        for( var i=0 ; i<recentSearchArray.length ; i++){
-            var recentSearchButton = $("<button>").attr("id", "recentSearchButton").text(recentSearchArray[i]);
-            recentSearchSection.append(recentSearchButton);
-        }
-
 ////////////////////////////////////
 // GET AJAX WEATHER DATA FOR CITY //
 ////////////////////////////////////
 
+    ajaxRequest = $("#searchInput").val();
+
     // Place Ajax Request in Query URL
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + ajaxRequest + "&appid=" + apiKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + ajaxRequest + "&units=imperial" + "&appid=" + apiKey;
+    
 
     $.ajax({
     url: queryURL,
-    method: "GET"
+    method: "GET"     
 
     }).then(function(response) {
+    console.log(ajaxRequest);
+    console.log(response);
 
-        // Collect Weather Icon, Temperature, Humidity and Wind Speed
-        weatherIcon = response.weather[0].icon;
-        tempNow = response.main.temp;
-        humidNow = response.main.humidity;
-        windNow = response.wind.speed;
+            // Set Up Array for Recent Searches
+            var recentSearchArray = [];
+            console.log(recentSearchArray);
+                        recentSearchArray.push(ajaxRequest);
+            console.log(ajaxRequest);
 
-        // Send Data to Local Storage
-        localStorage.setItem("weatherIcon", JSON.stringify(weatherIcon));
-        localStorage.setItem("tempNow", JSON.stringify(tempNow));
-        localStorage.setItem("humidNow", JSON.stringify(humidNow));
-        localStorage.setItem("windNow", JSON.stringify(windNow));
-        localStorage.setItem("cityName", JSON.stringify(ajaxRequest));
+            //Create List of Buttons for Recent Searches
+            for( var i=0 ; i<recentSearchArray.length ; i++){
+                var recentSearchButton = $("<button>").attr("id", "recentSearchButton")
+                recentSearchButton.text(recentSearchArray[i]);
+                recentSearchSection.append(recentSearchButton);
+                console.log(recentSearchArray[i])
+            }
 
-        //Collect City Coordinates for for Second Ajax Request to GET UV Index
-        latC = response.coord.lat;
-        lonC = response.coord.lon;
+            $("#navBar").on("click", function(e){
+                e.preventDefault;
 
-        // Set Up Request for UV Index at City Coordinates
-         var cityCoordinates = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latC + "&lon=" + lonC + "&appid=" + apiKey;
+                if (e.target.matches("button")){
+                    ajaxRequest = e.target.textContent
+                    console.log(ajaxRequest)
+                    searchButtonFunction(e);
+                };
 
-        $.ajax({
-            url: cityCoordinates,
-            method: "GET"
-        
-            }).then(function(response) {
+            })
+
+            // Collect Weather Icon, Temperature, Humidity and Wind Speed
+            cityName = ajaxRequest;
+            weatherIconCode = response.weather[0].icon;
+            tempNow = response.main.temp;
+            humidNow = response.main.humidity;
+            windNow = response.wind.speed;
+
+            // Send Data to Local Storage
+            localStorage.setItem("weatherIconCode", JSON.stringify(weatherIconCode));
+            localStorage.setItem("tempNow", JSON.stringify(tempNow));
+            localStorage.setItem("humidNow", JSON.stringify(humidNow));
+            localStorage.setItem("windNow", JSON.stringify(windNow));
+            localStorage.setItem("cityName", JSON.stringify(ajaxRequest));
+
+            //Collect City Coordinates for for Second Ajax Request to GET UV Index
+            latC = response.coord.lat;
+            lonC = response.coord.lon;
+
+            // Set Up Request for UV Index at City Coordinates
+            var cityCoordinates = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latC + "&lon=" + lonC + "&appid=" + apiKey;
+
+            $.ajax({
+                url: cityCoordinates,
+                method: "GET"
             
-            // Collect UVIndex and Send to Storage
-            uvIndex = response.value;
-            localStorage.setItem("uvIndex", JSON.stringify(uvIndex));
+                }).then(function(response) {
+                
+                // Collect UVIndex and Send to Storage
+                uvIndex = response.value;
+                localStorage.setItem("uvIndex", JSON.stringify(uvIndex));
 
+/////////////////////////////////////
+// CREATE PRIMARY ELEMENTS OF PAGE //
+/////////////////////////////////////
+
+                // Set Dates
+                //var currentDate = (moment().format("dddd, MMMM Do YYYY"));
+                //console.log(currentDate)
+
+                //Build Headline
+                
+                var cityHeadline = $("<h2>")
+                cityHeadline.text(cityName)
+                primarySection.append(cityHeadline);
+                
+                weatherIcon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + weatherIconCode + ".png")
+                weatherIcon.attr("id", "weatherIcon");
+                cityHeadline.after(weatherIcon)
+                
+                var tempHeadline = $("<h4>");
+                tempHeadline.text("Temperature: " + tempNow + "° Fahrenheit");
+                tempDisplay.append(tempHeadline);
+                console.log(tempNow);
+
+                var humidHeadline = $("<h4>");
+                humidHeadline.text("Humidity: " + humidNow + "%");
+                humidDisplay.append(humidHeadline);
+                console.log(humidNow);
+
+                var windHeadline = $("<h4>");
+                windHeadline.text("Wind Speed: " + windNow + "MPH");
+                windDisplay.append(windHeadline);
+                console.log(uvIndex);
+
+                var uvHeadline = $("<h4>");
+                uvHeadline.text("UV Index: " + uvIndex);
+                uvDisplay.append(uvHeadline);
+                console.log(uvIndex);
+
+///////////////////////////
+// COLLECT FORECAST DATA //
+///////////////////////////
+
+
+            });
+
+            // Place Ajax Request in Query URL
+            var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial" + "&appid=" + apiKey;
+
+
+            $.ajax({
+            url: queryURL,
+            method: "GET"     
+
+            }).then(function(response) {
+            console.log(ajaxRequest);
+            console.log(response);
+
+            allForecastIcons=[];
+            allForecastTemps=[];
+            allForecastHumidity=[];
+            
+            for(var i =0; i<6 ; i++){     
+                allForecastTemps.push(response.list[i].main.temp);
+                allForecastHumidity.push(response.list[i].main.humidity);
+                allForecastIcons.push(response.list[i].weather[0].icon);
+                }
+            
+
+//////////////////////////////////
+// CREATE FORECAST WEATHER PAGE //
+//////////////////////////////////
+
+            for(var i=0; i<6; i++){
+
+                day1Icon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + allForecastIcons[i] + ".png")
+                console.log(allForecastIcons[i])
+                day1Icon.attr("class", "dailyIcons");
+                $("#day"+ i) .append(day1Icon)
+
+                day1Temp = $("<h5>")
+                day1Temp.text("Temp: " + allForecastTemps[i] + "° F")
+                $("#day" + i).append(day1Temp);
+        
+                day1Humidity = $("<h5>");
+                day1Humidity.text("Humidity: " + allForecastHumidity[i] + "%");
+                $("#day" + i).append(day1Humidity);
+
+            };
+            
         });
-    
+
+
     });
 
 };
     
 /////////////////////////////////
-// CREATE PRIMARY WEATHER PAGE //
+// AUTO LOAD //
 /////////////////////////////////
 
 // Store Data from Local Storage Into Variables
@@ -138,21 +256,41 @@ console.log(storedWindNow);
 var storedUVNow = JSON.parse(localStorage.getItem("uvIndex"));
 console.log(storedUVNow);
 
-var cityHeadline = $("<h2>");
-
-if(ajaxRequest=""){
-    cityHeadline.text(ajaxRequest);
-} else if(ajaxRequest = null){
-    cityHeadline = storedCIty;
-    cityHeadline.textContent;
-};
 
 
-// $("div.something h1").text("hello").addClass("classname")
+var openingCityHeadline = $("<h2>")
+openingCityHeadline.text(storedCity)
+primarySection.append(openingCityHeadline);
+
+var openingweatherIcon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + storedWeatherNow + ".png")
+openingweatherIcon.attr("id", "weatherIcon");
+openingCityHeadline.after(openingweatherIcon)
+
+var openingtempHeadline = $("<h4>");
+openingtempHeadline.text("Temperature: " + storedTempNow + "° Fahrenheit");
+tempDisplay.append(openingtempHeadline);
+console.log(tempNow);
+
+var openinghumidHeadline = $("<h4>");
+openinghumidHeadline.text("Humidity: " + storedHumidNow + "%");
+humidDisplay.append(openinghumidHeadline);
+console.log(humidNow);
+
+var openingwindHeadline = $("<h4>");
+openingwindHeadline.text("Wind Speed: " + storedWindNow + "MPH");
+windDisplay.append(openingwindHeadline);
+console.log(uvIndex);
+
+var openinguvHeadline = $("<h4>");
+openinguvHeadline.text("UV Index: " + storedUVNow);
+uvDisplay.append(openinguvHeadline);
+console.log(uvIndex);
 
 
-primarySection.append(cityHeadline);
-console.log(cityHeadline);
-// primarySection.
+
+
+cityHeadline
+weatherIcon
+tempHeadline
 
 });
